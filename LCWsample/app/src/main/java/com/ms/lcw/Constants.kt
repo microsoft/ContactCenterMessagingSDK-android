0407e360245fa61cbd224e292f14e25f52f7595d
pipeline <pipeline@nuance.com>
2025-09-24 15:31:46 +0530

    creds removed

--- LCWsample/app/src/main/java/com/ms/lcw/Constants.kt
+++ LCWsample/app/src/main/java/com/ms/lcw/Constants.kt
@@ -1,8 +1,8 @@
 package com.ms.lcw
 
 object Constants {
-    const val orgId="b8fc9ed1-e960-f011-8ee5-6045bd080c2a"
-    const val orgUrl = "https://m-b8fc9ed1-e960-f011-8ee5-6045bd080c2a.us.omnichannelengagementhub.com"
-    const val orgWidgetId = "08222488-4a08-4ea2-8daa-0c448ff3aebb"
+    const val orgId=""
+    const val orgUrl = ""
+    const val orgWidgetId = ""
     const val authTkn = ""
 }
